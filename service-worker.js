aabd3d36eea36f61a7170c0de98c48cf4ae8d148
hanks-io <hanks19970810@gmail.com>
2024-11-11 17:09:06 +0530

    fix: pwa dome

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pwa-demo-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './manifest.json',
+    './icons/icon-192x192.svg',
+    './icons/icon-512x512.svg'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+}); 
